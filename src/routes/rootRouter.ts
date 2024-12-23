8db287068e698b6be4423af0af80c9ebecaa6e54
Nasaee <nasaee.dev@gmail.com>
2024-12-23 17:37:28 +0700

    first commit

--- src/routes/rootRouter.ts
+++ src/routes/rootRouter.ts
@@ -1,9 +1,13 @@
 import { Router } from 'express';
+import authRouter from './auth/auth.route';
 
 const rootRouter = Router();
 
 rootRouter.get('/', (req, res) => {
   res.send('Hello World');
 });
+
+//* Auth
+rootRouter.use('/auth', authRouter);
 
 export default rootRouter;
